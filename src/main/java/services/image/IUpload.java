1a20405b47f313d9d93cea5cbe860dcab26208da
CaoThanhNammm <21130448@st.hcmuaf.edu.vn>
2024-03-24 21:47:23 +0700

    thay đổi cách lấy ảnh của sản phẩm thông qua clound

--- src/main/java/services/image/IUpload.java
+++ src/main/java/services/image/IUpload.java
@@ -6,13 +6,13 @@ import java.util.List;
 //Interface dùng để quản lý các services upload ảnh
 //Mở rộng các services upload ảnh khác nhau, nếu
 public interface IUpload {
-    String getImage(String folderPath, String imageName);
+    String getImage(String imageName);
 
-    List<String> getImages(String folderPath, String[] imageNameArray);
+    List<String> getImages(String[] imageNameArray);
 
-    void uploadImage(String folderName, String imageName, Part part) throws Exception;
+    void uploadImage(String imageName, Part part) throws Exception;
 
-    void uploadImages(String folderName, String imageName, Part[] parts) throws Exception;
+    void uploadImages(String imageName, Part[] parts) throws Exception;
 
-    void createFolder(String folderName) throws Exception;
+    void createFolder() throws Exception;
 }
