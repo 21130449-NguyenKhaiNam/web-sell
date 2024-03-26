cdd8c89e6622e05c0f0d5852cd1d23035297ce0b
21130449-NguyenKhaiNam <21130449@st.hcmuaf.edu.vn>
2024-03-26 09:09:12 +0700

    Xử lý việc gọi Log #1: Thêm các file cần thiết cho việc gọi tự động

--- src/main/java/dao/CategoryDAO.java
+++ src/main/java/dao/CategoryDAO.java
@@ -5,7 +5,7 @@ import models.Parameter;
 
 import java.util.List;
 
-public class CategoryDAO {
+public class CategoryDAO implements IDao {
     public List<Category> getAllCategory() {
         StringBuilder sql = new StringBuilder();
         sql.append("SELECT id, nameType ")
