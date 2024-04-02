264132b5e2667652c5703e18439ca4a6c3f5418a
21130449-NguyenKhaiNam <21130449@st.hcmuaf.edu.vn>
2024-04-02 07:18:55 +0700

    Xử lý log #4: Điều chỉnh qui tắc lưu trữ của LogService

--- src/main/java/annotations/LogTable.java
+++ src/main/java/annotations/LogTable.java
@@ -1,10 +1,14 @@
-package dao;
+package annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Dùng cho việc đặt tên nhóm của bảng khi ghi xuống database.
+ * Sử dụng bằng các ghi thêm vào tại lớp muốn ghi xuống cơ sở dữ liệu
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 public @interface LogTable {
--- src/main/java/dao/ILogDAO.java
+++ src/main/java/dao/ILogDAO.java
@@ -1,11 +1,12 @@
 package dao;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 public interface ILogDAO {
-    void writeLog(String ip, int level, Map<String, String> params, LocalDate update, int table) throws JsonProcessingException;
+    void writeLog(ObjectMapper mapper, String ip, int level, Map<String, String> params, LocalDate update, int table) throws JsonProcessingException;
 }
