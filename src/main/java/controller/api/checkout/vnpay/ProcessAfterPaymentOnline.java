e01364f96dec778468248d5a66399830a9afecf2
21130449-NguyenKhaiNam <21130449@st.hcmuaf.edu.vn>
2024-06-04 18:36:20 +0700

    Xử lý thanh toán online, phát hiện lỗi bất đồng logic giữa cart và session của nó

--- src/main/java/controller/api/checkout/vnpay/ProcessAfterPaymentOnline.java
+++ src/main/java/controller/api/checkout/vnpay/ProcessAfterPaymentOnline.java
@@ -1,6 +1,11 @@
 package controller.api.checkout.vnpay;
 
+import com.google.gson.Gson;
 import config.ConfigPage;
+import controller.web.checkout.SuccessOrderController;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import models.DeliveryMethod;
 import models.PaymentMethod;
 import models.User;
@@ -33,14 +38,22 @@ public class ProcessAfterPaymentOnline extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int vnp_TransactionStatus = Integer.parseInt(request.getParameter("vnp_TransactionStatus"));
 
-//        if(vnp_TransactionStatus == 00){
-//            HttpSession session = request.getSession();
-//            User user = SessionManager.getInstance(request, response).getUser();
-//            String userIdCart = String.valueOf(user.getId());
-//
-//            ShoppingCart cart = (ShoppingCart) session.getAttribute(userIdCart);
+        if(vnp_TransactionStatus == 00){
+            HttpSession session = request.getSession();
+            User user = SessionManager.getInstance(request, response).getUser();
+            String userIdCart = String.valueOf(user.getId());
+
+            ShoppingCart cart = (ShoppingCart) session.getAttribute(userIdCart);
 //            int totalPrice = (int)cart.getTotalPrice(true);
-//
+            Gson gson = new Gson();
+            // Chuyển đổi json sang obj
+            OrderSuccess order = gson.fromJson((String) session.getAttribute("orders"), OrderSuccess.class);
+            TempOrder[] tempOrders = order.getOrders();
+            int totalPrice = 0;
+            for (int i = 0; i < tempOrders.length; i++) {
+                totalPrice += tempOrders[i].getPrice();
+            }
+
 //            int invoiceNo = Integer.parseInt(request.getParameter("invoiceNo"));
 //            String dateOrder = LocalDate.now().toString();
 //            String fullNameBuyer = cart.getDeliveryInfo().getFullName();
@@ -77,8 +90,45 @@ public class ProcessAfterPaymentOnline extends HttpServlet {
 //            session.removeAttribute("promotionCode");
 //            session.removeAttribute("failedApply");
 //            session.removeAttribute("successApplied");
+            session.removeAttribute("promotionCode");
+            session.removeAttribute("failedApply");
+            session.removeAttribute("successApplied");
+            request.setAttribute("invoiceNo", order.getInvoiceNo());
+            // Cần điều chỉnh khi có voucher
+            CheckoutServices.getINSTANCE().addNewOrder(order.getInvoiceNo(), user.getId(), order.getDateOrder(), user.getFullName(), user.getEmail(), user.getPhone(), user.getAddress(), order.getDelivery(), order.getPayment(), null);
+            for (int i = 0; i < tempOrders.length; i++) {
+                cart.remove(tempOrders[i].getId(), tempOrders[i].getInd());
+                CheckoutServices.getINSTANCE().addEachOrderDetail(order.getInvoiceNo(), tempOrders[i].getId(), tempOrders[i].getName(), tempOrders[i].getSize(), tempOrders[i].getColor(), tempOrders[i].getCount(), tempOrders[i].getPrice());
+            }
+            session.setAttribute(userIdCart, cart);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(ConfigPage.USER_SUCCESS_ORDER);
+            requestDispatcher.forward(request, response);
         }
 //
 //        request.getRequestDispatcher(ConfigPage.PRODUCT_BUYING).forward(request, response);
-//    }
+    }
+
+    @Data
+    @Getter
+    @NoArgsConstructor
+    class OrderSuccess {
+        private int delivery;
+        private int payment;
+        private TempOrder[] orders;
+        private int invoiceNo;
+        private String dateOrder;
+    }
+
+    @Data
+    @Getter
+    @NoArgsConstructor
+    class TempOrder {
+        private int id;
+        private int ind;
+        private String name;
+        private String color;
+        private String size;
+        private int count;
+        private double price;
+    }
 }
