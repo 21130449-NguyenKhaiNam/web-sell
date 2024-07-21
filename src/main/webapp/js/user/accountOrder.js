0776e5fd826c56b2c4ab1d258246572c1b402d9c
21130449-NguyenKhaiNam <21130449@st.hcmuaf.edu.vn>
2024-07-21 13:57:31 +0700

    Update show total price for view order

--- src/main/webapp/js/user/accountOrder.js
+++ src/main/webapp/js/user/accountOrder.js
@@ -4,6 +4,7 @@ import {getFeeAndLeadTime} from "../shipping.js";
 $(document).ready(function () {
     // Lấy ra trạng thái đơn hàng chưa xác nhận khi mới vào trang
     let statusId = 1;
+    let totalPrice = 0;
     getOrders(statusId);
     $('.list-group-item-action').click(function () {
         $('.list-group-item-action').removeClass('active');
@@ -74,6 +75,7 @@ $(document).ready(function () {
             const temporary = order.items.reduce(function (total, item) {
                 return total + item.price * item.quantity;
             }, 0);
+            totalPrice += temporary;
             modal.find("#order__temporary").text(formatCurrency(temporary));
         }
 
@@ -96,9 +98,11 @@ $(document).ready(function () {
         console.log(JSON.stringify(order.address))
         getFeeAndLeadTime(order.address).then(data => {
             console.log(data.feeShipping, data.leadDate);
+            totalPrice -= data.feeShipping
             modal.find("#order__shipping-fee").text(formatCurrency(data.feeShipping));
             modal.find("#order__lead-date").text(convertUnixToDate(data.leadDate));
         })
+        modal.find("#order__total").text(formatCurrency(totalPrice))
     }
 
     function addEventViewDetail() {
